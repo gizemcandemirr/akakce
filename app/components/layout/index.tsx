b734725dcb700e2a92f20ed20f7237a751a5e516
Gizem Candemir <gizem.candemir@marketcalculus.ai>
2023-09-06 19:26:51 +0300

    .

--- app/components/layout/index.tsx
+++ app/components/layout/index.tsx
@@ -7,7 +7,7 @@ interface LayoutProps {
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
-    <div className='mx-auto h-screen'>
+    <div className='mx-auto h-screen bg-gray-100'>
       <Header />
       <main>{children}</main>
     </div>
